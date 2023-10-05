12730698fe336fb6933c4269f6ea44632b160eb2
Fernando Ubuntu VIT <chinos4944@gmail.com>
2023-10-05 13:33:11 -0400

    agregado enums

---
+++ RelacionesEjEx4/src/relacionesejex4/enumeraciones/Apellido.java
@@ -0,0 +1,7 @@
+package relacionesejex4.enumeraciones;
+
+public enum Apellido {
+
+    Velasquez, Perez, Figueroa, Carmona, Suarez, Sanchez, Laurente, Parente, Hernandez, Fernandez;
+    
+}
---
+++ RelacionesEjEx4/src/relacionesejex4/enumeraciones/Nombre.java
@@ -0,0 +1,7 @@
+package relacionesejex4.enumeraciones;
+
+public enum Nombre {
+
+    Jose, Carmen, Andres, Cesar, Javier, Fernando, Stefany, Thomas, Alberto, Juan, Mathias;
+    
+}
